0fe717d978960f59ec25c4d94e163548bcb7d063
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-12-11 15:37:11 +0000

    Added experience 4 remaining commands

--- exp4/router.sh
+++ exp4/router.sh
@@ -5,5 +5,5 @@
 /ip address add address=172.16.1.49/24 interface=ether1
 /ip address add address=172.16.41.254/24 interface=ether2
 
-# If it's not already defined
 /ip route add dst-address=172.16.40.0/24 gateway=172.16.41.253
+/ip route add dst-address=0.0.0.0/0 gateway=172.16.1.254
--- exp4/tux43.sh
+++ exp4/tux43.sh
@@ -3,5 +3,4 @@
 
 ifconfig eth0 172.16.40.1/24
 
-# If it's not already defined
 route add default gw 172.16.40.254
